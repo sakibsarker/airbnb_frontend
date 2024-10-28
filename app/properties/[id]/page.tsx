f47ea92502d6e70a71af09a98d91db6731cf790c
sakibsarker <sakib15-3824@diu.edu.bd>
2024-10-29 01:27:25 +0600

    details page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,43 +1,48 @@
 import React from "react";
 import Image from "next/image";
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
-const PropertyDetailPage = () => {
+import apiService from "@/app/components/services/apiService";
+const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
+  const property = await apiService.get(`/api/properties/${params.id}`);
+  console.log("DATA: ", JSON.stringify(property));
+
   return (
     <main className="max-w=[1500px] mx-auto px-6 pb-6">
       <div className="w-full h-[64vh] mb-5 overflow-hidden rounded-xl relative">
         <Image
-          alt="Beach House"
+          alt={property.data.title}
           fill
-          src="/image/house.jpg"
+          src={property.data.image_url}
           className="object-cover w-full h-full"
         />
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
         <div className="col-span-3 py-6 pr-6">
-          <h1 className="mb-4 text-4xl">Property name</h1>
+          <h1 className="mb-4 text-4xl">{property.data.title}</h1>
           <span className="mb-6 block text-lg text-gray-600">
-            4 guests - 2 bedrooms - 1
+            {property.data.guests} guests - {property.data.bedrooms} bedrooms -{" "}
+            {property.data.bathrooms} bathrooms
           </span>
           <hr />
           <div className="py-6 flex items-center space-x-4">
-            <Image
-              alt="Profile"
-              width={50}
-              height={50}
-              src="/image/house.jpg"
-              className="rounded-full object-cover"
-            />
+            {property.data.landlord.avatar_url && (
+              <Image
+                alt="Profile"
+                width={50}
+                height={50}
+                src={property.data.landlord.avatar_url}
+                className="rounded-full object-cover"
+              />
+            )}
             <p>
-              <strong>Sakib Sarker</strong> is your host
+              <strong>{property.data.landlord.name}</strong> is your host
             </p>
           </div>
           <hr />
-          <p className="mt-6 text-lg">
-            Loren ldjflajfkjdlk flajdfl aj jfdlajf ajsdfl aldfjls
-          </p>
+          <p className="mt-6 text-lg">{property.data.description}</p>
         </div>
-        <ReservationSidebar />
+        <ReservationSidebar property={property} />
       </div>
     </main>
   );
